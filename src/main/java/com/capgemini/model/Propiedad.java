850d53cdd58e49fa46174d8477e03de7e115dd52
ableortu <ableortu@LES004822.corp.capgemini.com>
2018-08-17 18:27:30 +0200

    Getter and setters

--- src/main/java/com/capgemini/model/Propiedad.java
+++ src/main/java/com/capgemini/model/Propiedad.java
@@ -1,10 +1,15 @@
 package com.capgemini.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.hibernate.validator.constraints.NotEmpty;
@@ -24,5 +29,83 @@ public class Propiedad {
 	@NotEmpty
 	@Column(name = "nombre", unique=true, nullable = false)
 	private String nombre;
+	
+	
+	@OneToMany(mappedBy="tipo_propiedad", fetch = FetchType.EAGER, cascade = {CascadeType.REMOVE, CascadeType.REMOVE})
+	private Propiedad propiedades; 
+	
+	@OneToMany(mappedBy="ubicacion", fetch = FetchType.EAGER, cascade = {CascadeType.REMOVE, CascadeType.REMOVE})
+	private Ubicacion ubicaciones;
+
+	public int getId_propiedad() {
+		return id_propiedad;
+	}
+
+	public void setId_propiedad(int id_propiedad) {
+		this.id_propiedad = id_propiedad;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Propiedad getPropiedades() {
+		return propiedades;
+	}
+
+	public void setPropiedades(Propiedad propiedades) {
+		this.propiedades = propiedades;
+	}
+
+	public Ubicacion getUbicaciones() {
+		return ubicaciones;
+	}
+
+	public void setUbicaciones(Ubicacion ubicaciones) {
+		this.ubicaciones = ubicaciones;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id_propiedad;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Propiedad other = (Propiedad) obj;
+		if (id_propiedad != other.id_propiedad)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Propiedad [id_propiedad=" + id_propiedad + ", direccion=" + direccion + ", nombre=" + nombre
+				+ ", propiedades=" + propiedades + ", ubicaciones=" + ubicaciones + "]";
+	} 
+	
+	
+	
 
 }
--- src/main/java/com/capgemini/model/Ubicacion.java
+++ src/main/java/com/capgemini/model/Ubicacion.java
@@ -1,10 +1,13 @@
 package com.capgemini.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.validator.constraints.NotEmpty;
@@ -20,5 +23,40 @@ public class Ubicacion {
 	@NotEmpty
 	@Column(name = "ubicacion", unique=true, nullable = false)
 	private String ubicacion;
+	
+	@ManyToOne(cascade = {CascadeType.REMOVE, CascadeType.REMOVE})
+	@JoinColumn(name = "ubicacion_id")
+	private Propiedad prop1;
+
+	public int getId_ubicacion() {
+		return id_ubicacion;
+	}
+
+	public void setId_ubicacion(int id_ubicacion) {
+		this.id_ubicacion = id_ubicacion;
+	}
+
+	public String getUbicacion() {
+		return ubicacion;
+	}
+
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+
+	public Propiedad getProp1() {
+		return prop1;
+	}
+
+	public void setProp1(Propiedad prop1) {
+		this.prop1 = prop1;
+	}
+
+	@Override
+	public String toString() {
+		return "Ubicacion [id_ubicacion=" + id_ubicacion + ", ubicacion=" + ubicacion + ", prop1=" + prop1 + "]";
+	}
+	
+
 
 }
